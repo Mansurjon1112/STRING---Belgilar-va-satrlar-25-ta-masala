4e98173f61828d7aa596b4febb0c55406e5953a1
Mansurjon1112 <64670025+Mansurjon1112@users.noreply.github.com>
2021-06-25 09:42:39 +0500

    Update String03.py

--- String03.py
+++ String03.py
@@ -9,6 +9,7 @@ tuzilsin.
 belgi = input('Belgi kiriting: ')
 
 if len(belgi)==1:
-    print(ord(belgi))
+    print('oldingi belgi:', chr(ord(belgi)-1))
+    print('oldingi belgi:', chr(ord(belgi)-1))
 else:
     print('Faqat 1 ta belgi kiriting')
