d4c6396be8f602e2c8df3d99ad96c543ee24c3b8
Mansurjon1112 <64670025+Mansurjon1112@users.noreply.github.com>
2021-06-25 09:54:30 +0500

    Update String03.py

--- String03.py
+++ String03.py
@@ -10,6 +10,6 @@ belgi = input('Belgi kiriting: ')
 
 if len(belgi)==1:
     print('oldingi belgi:', chr(ord(belgi)-1))
-    print('oldingi belgi:', chr(ord(belgi)-1))
+    print('oldingi belgi:', chr(ord(belgi)+1))
 else:
     print('Faqat 1 ta belgi kiriting')
