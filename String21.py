a6314be48c60300c6d2e08f022349c3823d164f5
Mansurjon Muhammadovich Kamolov <64670025+Mansurjon1112@users.noreply.github.com>
2021-07-02 11:05:34 +0500

    Update String21.py

--- String21.py
+++ String21.py
@@ -9,8 +9,11 @@ Created on Wed Jun 23 20:19:59 2021
 
 @author: Mansurjon Kamolov
 """
+satr = input('Musbat son kiriting: ')
+for i in satr[::-1]:
+    print(i, end = '')
 
-
+"""
 satr = input('Musbat son kiriting: ')
 yangi=''
 index=0
@@ -20,3 +23,4 @@ for i in satr[0:-1]:
     
 yangi += satr[0]
 print(yangi)
+"""
