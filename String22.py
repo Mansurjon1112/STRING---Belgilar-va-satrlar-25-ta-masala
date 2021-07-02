2171c6d4e5eac9b2cd82ae4a569b64bdddd931e2
Mansurjon Muhammadovich Kamolov <64670025+Mansurjon1112@users.noreply.github.com>
2021-07-02 11:22:39 +0500

    Update String22.py

--- String22.py
+++ String22.py
@@ -8,10 +8,18 @@ Created on Wed Jun 23 20:24:51 2021
 @author: Mansurjon Kamolov
 """
 
+#1-usul
 son = int(input("Butun son kiriting "))
 
 summa = 0
 while (son != 0):
     summa = summa + son % 10
     son = son // 10
+print("Raqamlari yig'indisi: ", summa)
+
+#2-usul
+son = input('Butun son kiriting ')
+summa=0
+for i in son:
+    sum+=int(i)
 print("Raqamlari yig'indisi: ", summa)
